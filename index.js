fb1035b0e2a2e06008f098648cb5c1912f8d7587
GeorgiKarchev1 <goshoo429@gmail.com>
2023-03-07 22:35:30 +0200

    Adding more stuff to the game

--- index.js
+++ index.js
@@ -15,13 +15,51 @@ let canvasPosition = canvas.getBoundingClientRect();
     click: false
   }
    canvas.addEventListener('mousedown', function(event){ 
+    mouse.click = true; 
     mouse.x = event.x - canvasPosition.left;
     mouse.y = event.y - canvasPosition.top;
    });
-
+   canvas.addEventListener('mouseup', function() {
+      mouse.click = false;
+   })
 // Player
  class Player {
-    
+  constructor(){
+    this.x = canvas.width;
+    this.y = canvas.height/2;
+    this.radius = 50;
+    this.angle = 0;
+    this.frameX = 0; 
+    this.frameY = 0;
+    this.frame = 0;
+    this.spriteWidth = 498;
+    this.spriteHeigh = 327;
+  }  
+  update() { 
+    const dx = this.x - mouse.x;
+    const dy = this.y - mouse.y;
+    if (mouse.x != this.x) { 
+         this.x -= dx/30; // Тва 30 ти е един вид дилей ,, ЗА ДА ИМА АНИМАЦИЯ"
+    }
+    if (mouse.y != this.y) { 
+        this.y -= dy/30;
+    }
+  }
+
+  draw() {
+    if (mouse.click) {
+        ctx.lineWidth = 0.2;
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(mouse.x, mouse.y);
+        ctx.stroke();
+    } 
+     // Sprite
+     ctx.fillStyle = 'red';
+     ctx.beginPath();
+  }
+
+
  }
 // Bubbles
 
